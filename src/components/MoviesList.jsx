90a5a2d38972e18597ff52c50afade71f6001374
MuhammadFahri <fahribl2014@gmail.com>
2023-10-18 15:09:00 +0700

    React Movies

---
+++ src/components/MoviesList.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import classes from './MoviesList.module.css';
+import { Movie } from './Movie';
+export const MoviesList = (props) => {
+  return (
+    <ul className={classes['movies-list']}>
+      {props.movies.map((movie) => (
+        <Movie
+          key={movie.id}
+          title={movie.title}
+          releaseDate={movie.releaseDate}
+          openingText={movie.openingText}
+        />
+      ))}
+    </ul>
+  )
+}
